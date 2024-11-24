4fd663cecb20f29f3770e324e6e8c8af57f328d0
João Vitor B. <jv23072006@gmail.com>
2024-11-24 16:06:47 -0300

    q16 Atualização

--- Exercicios/Lista03/q16.asm
+++ Exercicios/Lista03/q16.asm
@@ -2,17 +2,51 @@
 main:
 	addi $2, $0, 5
 	syscall
-	add $8, $0, $2 # Inicializador
+	add $7, $0, $2
 	addi $2, $0, 5
 	syscall
-	add $9, $0, $2 
-	add $9, $9, 1 # Condição do Loop
-	addi $2, $0, 1
-loop:
-	beq $8, $9 print
+	add $6, $0, $2
+	jal primo
 print:
-	add $4, $0
+	addi $4, $0, '\n'
+	addi $2, $0, 11
+	syscall
+	add $4, $0, $3
+	addi $2, $0, 1
 	syscall
 end:
 	addi $2, $0, 10
 	syscall
+
+#=============================================
+# Função para Saber quantos Primos tem de 
+# A a B
+# Entradas: $7, $6 (A, B)
+# Saidas: $3
+# Registradores Sujos: 
+
+primo:
+	addi $8, $0, 1
+	beq $7, $6 endi
+	beq $7, $8 yes
+	addi $8, $0, 2
+loop:
+	beq $7, $8 yes
+	div $7, $8
+	addi $8, $8, 1
+	mfhi $9
+	bne $9, $0 loop
+	addi $7, $7, 1
+	j primo
+yes:
+	add $3, $3, 1
+	add $4, $0, $7
+	addi $2, $0, 1
+	syscall
+	addi $4, $0, ' '
+	addi $2, $0, 11
+	syscall
+	addi $7, $7, 1
+	j primo
+endi:
+	jr $31
