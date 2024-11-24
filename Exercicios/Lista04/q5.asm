19bcc169cbaf52a331f9794e9090e869704a76e8
João Vitor B. <jv23072006@gmail.com>
2024-11-24 17:10:35 -0300

    q5 Atualização

--- Exercicios/Lista04/q5.asm
+++ Exercicios/Lista04/q5.asm
@@ -3,15 +3,44 @@ main:
 	addi $2, $0, 5
 	syscall
 	add $7, $0, $2
-	jal square
+	jal perfect
+print:
+	beq $3, $0 no
+	addi $4, $0, 'S'
+	addi $2, $0, 11
+	syscall
+	j end
+no:
+	addi $4, $0, 'N'
+	addi $2, $0, 11
+	syscall
 end:
 	addi $2, $0, 10
 	syscall
 	
-#====================================================================
-# Função para Imprimir se um Número é um Quadrado Perfeito
-# Entrada: $7
-# Saida: ---
-# Registradores Sujos:
+#=============================================
+# Função para Saber se um Valor é um 
+# Quadrado Perfeito
+# Entradas: $7
+# Saidas: $3
+# Registradores Sujos: $8, $9, $10
 
+perfect:
+	addi $8, $0, 2 
+	div $7, $8 # Dividir N por 2
+	mflo $9
+	addi $9, $9, 1 # Metade de N + 1
+	addi $8, $0, 1 # AUX
 square:
+	mul $10, $8, $8 # AUX * AUX
+	beq $10, $7 yes # AUX = N (É Primo)
+	addi $8, $8, 1 # AUX + 1
+	beq $8, $9 noi # AUX = Metade de N + 1 (Não é Primo)
+	j square
+yes:
+	addi $3, $0, 1
+	j endi
+noi:
+	addi $3, $0, 0
+endi:
+	jr $31
